722b10feb3d4ad0d7c55795f23008938114b474f
OriishiTakahiro <takahiro0914@live.jp>
2019-04-14 00:38:40 +0900

    fix directions of symbolic link

--- mklinks.sh
+++ mklinks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-ln -s $HOME/.zlogin $HOME/.zsrc/.zlogin
-ln -s $HOME/.zlogout $HOME/.zsrc/.zlogout
-ln -s $HOME/.zprofile $HOME/.zsrc/.zprofile
-ln -s $HOME/.zshenv $HOME/.zsrc/.zshenv
-ln -s $HOME/.zshrc $HOME/.zsrc/.zshrc
+ln -s $HOME/.zsrc/.zlogin $HOME/.zlogin
+ln -s $HOME/.zsrc/.zlogout $HOME/.zlogout 
+ln -s $HOME/.zsrc/.zprofile $HOME/.zprofile 
+ln -s $HOME/.zsrc/.zshenv $HOME/.zshenv 
+ln -s $HOME/.zsrc/.zshrc $HOME/.zshrc
